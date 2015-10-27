6954132059178794359733be8df283fbaa67cea5
Josh Hawkins <jkhaccounts@gmail.com>
2015-10-26 23:00:40 -0500

    Consolidated JS; Fixed manifest
    Chrome extension now works as expected!

--- js/listeners.js
+++ js/listeners.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    $('#slot2input').focus();
     $('#slot3').gCalFlow({
         calid: calConfig.calid,
         maxitem: 3
     });
+});
+
+document.addEventListener("submit", function closetab() {
+    setTimeout(function() {
+        close();
+    }, 1);
 });
