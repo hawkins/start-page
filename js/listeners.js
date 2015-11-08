b4002748a6b69888388d96d955187a2fe248ba21
Josh Hawkins <jkhaccounts@gmail.com>
2015-11-07 19:51:04 -0600

    +API Key and -docs

--- js/listeners.js
+++ js/listeners.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $('#slot2input').focus();
     $('#slot3').gCalFlow({
         calid: calConfig.calid,
+        apikey: "AIzaSyCae639_0w0PW-L_QBlvlw2VTVNWmTtSNk",
         maxitem: 3
     });
 });
