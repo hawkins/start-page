634a2768bf1599cc74de71cd8c2bf24776d62369
Josh Hawkins <jkhaccounts@gmail.com>
2016-01-30 23:03:29 -0600

    Added header saving, changed default settings improved first time setup
    I think we're ready for release!

--- js/listeners.js
+++ js/listeners.js
@@ -3,7 +3,24 @@ var calConfig;
 function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
+// Configure Google calendar
+function loadCalendar(calid, numberEvents) {
+    // Default parameters
+    if (calid === undefined) {
+        calid = calConfig;
+    }
+    if (numberEvents === undefined) {
+        numberEvents = 3;
+    }
+    // Load the calendar now
+    $('#slot3').gCalFlow({
+        calid: calid,
+        apikey: "AIzaSyCae639_0w0PW-L_QBlvlw2VTVNWmTtSNk",
+        maxitem: numberEvents
+    });
+}
 document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("Hey developers! If you're interested to contribute, I gladly accept pull requests at https://github.com/hawkins/start-page/ ! :) -hawkins")
     $('#slot2input').focus();
 
     //// Read Google Calendar
@@ -19,11 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             $('#btn-calendar').click();
         } else {
             // Configure Google calendar
-            $('#slot3').gCalFlow({
-                calid: calConfig,
-                apikey: "AIzaSyCae639_0w0PW-L_QBlvlw2VTVNWmTtSNk",
-                maxitem: 3
-            });
+            loadCalendar();
         }
     });
 
@@ -53,6 +66,7 @@ $('#slot2form').submit(function(ev) {
 // Define last clicked link for use in "passing" as parameter to popup form
 var lastClickedLinkAddress;
 var lastClickedLinkTitle;
+var lastClickedHeader;
 
 if (document.addEventListener) {
     document.addEventListener('contextmenu', function(e) {
@@ -60,7 +74,7 @@ if (document.addEventListener) {
         // But let's only let it work on anchors
         if ('A' == e.path[0].tagName) {
             e.preventDefault();
-            console.log(e.path[0]);
+            // console.log(e.path[0]);
             if (hasClass(e.path[0].parentElement, "gcf-title")) {
                 // Open the popup to set up calendar
                 $('#btn-calendar').click();
@@ -69,11 +83,17 @@ if (document.addEventListener) {
             // Since we're not a descendent of slot3, open link configuration
             lastClickedLink = e.path[0];
             // Load the link's href and text into the popup
-            console.log(lastClickedLink);
+            // console.log(lastClickedLink);
             $("#link-title").val(lastClickedLink.innerHTML);
             $("#link-address").val(lastClickedLink.href);
             // Now open the popup
             $("#btn-link").click();
+        }
+        if ('H2' == e.path[0].tagName) {
+            e.preventDefault();
+            lastClickedHeader = e.path[0];
+            $('#header-name').val(lastClickedHeader.innerHTML)
+            $('#btn-header').click();
         }
     }, false);
 } else {
@@ -135,6 +155,7 @@ $('#popupform2').PopupForm({
             console.log("Saved calendar to chrome.storage.");
             $('#calendar-id').val(calConfig);
         });
+        loadCalendar($('#calendar-id').val());
     },
     submitSuccess: function(data) {
         console.log('Form submitted successfully.')
@@ -145,5 +166,36 @@ $('#popupform2').PopupForm({
     closed: function() {
         console.log('Form closed.')
         $('#calendar-id').val(calConfig);
+    }
+});
+// Header customization popup
+$('#popupform3').PopupForm({
+    openPopupButton: $('#btn-header')[0],
+    formTitle: 'Edit Header Name',
+
+    validateStepOne: function (container) {
+        if($('#header-name').val().length > 0) {
+            return true;
+        } else {
+            alert('Header name is a required field');
+            return false;
+        }
+    },
+    submitted: function() {
+        // Save the calendar ID in chrome.storage
+        var newHeaderName = $('#header-name').val().toString();
+        var headerID = lastClickedHeader.id.toString();
+        var saveHeader = {headerID: newHeaderName};
+        $("div h2#"+headerID).text(newHeaderName);
+        saveSettings();
+    },
+    submitSuccess: function(data) {
+        console.log('Form submitted successfully.')
+    },
+    submitFailed: function(xhr) {
+        console.log('Form submit failed.')
+    },
+    closed: function() {
+        console.log('Form closed.')
     }
 });
--- js/options.js
+++ js/options.js
@@ -1,7 +1,9 @@
 var saveLinks = {};
 var saveTitles = {};
+var saveHeaders = {};
 function saveSettings() {
     console.log("Saving...");
+    // Save links
     for (i = 1; i <= 12; i++) {
         // Define variables
         var id = "#l" + i.toString()
@@ -14,28 +16,6 @@ function saveSettings() {
         }
         saveLinks[linkid] = $(id).attr("href");
         saveTitles[titleid] = $(id).html();
-
-        // // Save in chrome.storage
-        // chrome.storage.sync.set(saveLinks, function () {
-        //     if (chrome.extension.lastError) {
-        //         alert('An error occurred: ' + chrome.extension.lastError.message);
-        //     }
-        //     chrome.storage.sync.get(linkid, function (result) {
-        //         console.log("save: ");
-        //         console.log(result);
-        //         console.log(result[linkid])
-        //     });
-        // });
-        // chrome.storage.sync.set(saveTitles, function () {
-        //     if (chrome.extension.lastError) {
-        //         alert('An error occurred: ' + chrome.extension.lastError.message);
-        //     }
-        //     chrome.storage.sync.get(titleid, function (result) {
-        //         console.log("save: ");
-        //         console.log(result);
-        //         console.log(result[titleid])
-        //     });
-        // });
     }
     console.log(saveLinks, saveTitles);
     // Save in chrome.storage
@@ -51,31 +31,59 @@ function saveSettings() {
             console.log("Please let the developer know at https://github.com/hawkins/start-page/issues");
         }
     });
+    // Save Headers
+    for (i = 1; i <= 3; i++) {
+        // Define Variables
+        var headerKey = "header" + i.toString();
+        var headerID = "#" + headerKey;
+        saveHeaders[headerKey] = $(headerID).text();
+    }
+    console.log(saveHeaders);
+    chrome.storage.sync.set(saveHeaders, function () {
+        if (chrome.extension.lastError) {
+            console.log('An error occurred: ' + chrome.extension.lastError.message);
+            console.log("Please let the developer know at https://github.com/hawkins/start-page/issues");
+        }
+    });
 }
 
 function loadSettings() {
+    // Load links
     for (var i = 1; i <= 12; i++) {
         // Define variables
         var id = "#l" + i.toString()
         var linkid = "l" + i.toString() + "link";
         var titleid = "l" + i.toString() + "title";
-        var re = /l\d+/; // Regular expression for detecting ID
+        var linkRe = /l\d+/; // Regular expression for detecting ID
         // Load from these keys
         chrome.storage.sync.get(linkid, function (result) {
             // Update the link now
             for (linkid in result) {
-                // console.log("Loaded: ");
-                // console.log(result[linkid]);
-                $("#"+re.exec(linkid)).attr("href", result[linkid]);
+                console.log("Loaded: ");
+                console.log(result[linkid]);
+                $("#"+linkRe.exec(linkid)).attr("href", result[linkid]);
             };
         });
         chrome.storage.sync.get(titleid, function (result) {
             // Set the title now
             for (titleid in result) {
-                // console.log("Loaded: ");
-                // console.log(result[titleid]);
-                $("#"+re.exec(linkid)).text(result[titleid]);
+                console.log("Loaded: ");
+                console.log(result[titleid]);
+                $("#"+linkRe.exec(linkid)).text(result[titleid]);
             };
         });
-    }
+    };
+    // Load headers
+    for (var i = 1; i <= 3; i++) {
+        var headerKey = "header" + i.toString();
+        var headerID = "#" + headerKey;
+        var headerRe = /header\d+/; // Regular expression for detecting ID
+        // Load from these keys
+        chrome.storage.sync.get(headerKey, function (result) {
+            // Update the link now
+            for (key in result) {
+                $("div h2#" + headerRe.exec(key)).text(result[key]);
+            };
+        });
+    };
 }
